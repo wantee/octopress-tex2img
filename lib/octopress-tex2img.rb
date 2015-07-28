c7c3cb79955346d0646601c01cb15ab756d9a40a
Wang Jian <wantee.wang@gmail.com>
2015-07-28 17:44:58 +0800

    finish tag

--- lib/octopress-tex2img.rb
+++ lib/octopress-tex2img.rb
@@ -1,13 +1,19 @@
 require 'octopress-tex2img/version'
 require 'octopress-ink'
 
+module OctopressTex2img
+  autoload :Tags,            'octopress-tex2img/tags'
+end
+
+Liquid::Template.register_tag('tex2img', OctopressTex2img::Tags)
+
 Octopress::Ink.add_plugin({
   name:          "Octopress Tex2img",
   slug:          "octopress-tex2img",
   gem:           "octopress-tex2img",
   path:          File.expand_path(File.join(File.dirname(__FILE__), "..")),
   version:       OctopressTex2img::VERSION,
-  description:   "",                                # What does your theme/plugin do?
-  source_url:    "https://github.com/wantee/octopress-tex2img", # <- Update info
-  website:       "https://github.com/wantee/octopress-tex2img"                                 # Optional project website
+  description:   "Generate image from tex source file and insert into markdown",
+  source_url:    "https://github.com/wantee/octopress-tex2img",
+  website:       "https://github.com/wantee/octopress-tex2img"
 })
---
+++ lib/octopress-tex2img/tags.rb
@@ -0,0 +1,43 @@
+# tex2img
+# 
+# usage {% tex2img path_to_tex args-to-img-tag %}
+# 
+
+require 'octopress-image-tag'
+
+module OctopressTex2img
+  class Tags < Liquid::Tag
+    def initialize(tag_name, markup, tokens)
+      super
+      @texpath, *params = markup.strip.split(/\s/)
+      if !File.exists?(@texpath)
+        raise ArgumentError.new("texfile[#{@texpath}] does not exist!")
+      end
+      @imgtag = Octopress::Tags::ImageTag::Tag.new(tag_name, markup, tokens)
+    end
+
+    def render(context)
+      attributes = @imgtag.image(context)
+      site = context.registers[:site]
+      pngpath = File.join(site.source, attributes['src'])
+      @texpath = File.join(site.source, @texpath)
+
+      if !File.exists?(pngpath) || File.stat(@texpath).mtime > File.stat(pngpath).mtime
+        puts "Generating #{pngpath} from #{@texpath}"
+        texdir = File.dirname(@texpath)
+        pngdir = File.dirname(pngpath)
+        FileUtils.mkdir_p(pngdir)
+        base = @texpath.sub(/.tex$/, '')
+        pdf = @texpath.sub(/.tex$/, '.pdf')
+      	system "xelatex -output-directory=#{texdir} --interaction=nonstopmode #{base} >/dev/null"
+      	system "convert -density 300 #{pdf} -quality 90 #{pngpath}"
+
+      	FileUtils.rm_f("#{base}.aux")
+      	FileUtils.rm_f("#{base}.log")
+      	FileUtils.rm_f("#{base}.pdf")
+      end
+      @imgtag.render(context)
+    end
+  end
+end
+
